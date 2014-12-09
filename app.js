041d7939d0ed09bb0db7fda85a36f447d90eb85d
adesuwau <missadeugbogbo@gmail.com>
2014-12-09 17:29:42 -0500

    Adds initial list controller

---
+++ app.js
@@ -0,0 +1,30 @@
+console.log('Linked.');
+
+(function(){
+var app = angular.module('list', [ ]);
+
+app.controller('ListController', function(){
+
+});
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+// $.ajax({
+//   type: "GET",
+//   url:"https://openapi.etsy.com/v2/listings/active?api_key={insert api key here}",
+//   dataType: "json",
+//   success: function(data){
+//     responseData = data;
+//     console.log(responseData);
+//   }
+// });
